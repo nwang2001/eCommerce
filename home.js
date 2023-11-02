0d97b3a5adb8766cb903163fa1b83e064669ea36
nwang2001 <nikkiw011201@gmail.com>
2023-11-02 15:17:36 -0400

    added home js file

---
+++ home.js
@@ -0,0 +1,16 @@
+var image = document.getElementById('image');
+var slides = ['slideimages/artdesk.jpeg', 'slideimages/artstore.jpeg', 'slideimages/beads.jpeg', 'slideimages/markercups.jpeg', 'slideimages/markers.jpeg', 'slideimages/anotherdesk.jpeg', 'slideimages/brushes.jpeg', 'slideimages/paints.webp'];
+var start = 0;
+
+function slider(){
+    if(start<slides.length){
+        start = (start + 1) % slides.length;
+    }
+    else{
+        start = 0;
+    }
+    console.log(image);
+    image.innerHTML = "<img src=" + slides[start] + ">";
+   
+}
+setInterval(slider, 2000);
